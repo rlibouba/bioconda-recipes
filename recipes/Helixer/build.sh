9d665e1199950d17f7c205aa6ebbc7b21732f704
rlibouban <romane.libouban@irisa.fr>
2023-04-21 10:26:22 +0200

    Update

--- recipes/Helixer/build.sh
+++ recipes/Helixer/build.sh
@@ -1,12 +1,7 @@
 !/bin/bash
 
-cd Helixer
-pip install .
 git clone https://github.com/TonyBolger/HelixerPost.git
 cd HelixerPost && cargo build --release
 
-#cp helixer_post_bin ~/miniconda3/envs/helixer_environement/bin/helixer_post_bin #
-p=`pwd`
-export PATH=$PATH:/$p/helixer_post_bin
-#export PATH="$p/helixer_post_bin:$PATH" 
-echo $PATH
+#cp /target/release/helixer_post_bin ~/miniconda3/envs/helixer_environement/bin/helixer_post_bin #
+cp target/release/helixer_post_bin $PREFIX/bin
